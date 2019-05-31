736df4e9d69ac91aaf1813b1e0e8a4b4532df01f
riceracer <riceracer>
2019-05-31 09:42:31 -0700

    Add basic overfitting, underfitting, regularization, dropout tutorial example.

---
+++ tensorflow/examples/basic_overfitting_underfitting.py
@@ -0,0 +1,208 @@
+"""
+ML basics tutorial: overfitting and underfitting
+https://www.tensorflow.org/alpha/tutorials/keras/overfit_and_underfit
+
+Tutorial explores using weight regularization and dropout as mechanisms to prevent
+overfitting.
+
+#@title MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# fix the allow pickle issue with numpy
+#https://stackoverflow.com/questions/55890813/how-to-fix-object-arrays-cannot-be-loaded-when-allow-pickle-false-for-imdb-loa
+# save np.load
+np_load_old = np.load
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+
+# Load IMDB dataset
+NUM_WORDS = 10000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+# restore np.load for future normal usage
+np.load = np_load_old
+
+def multi_hot_sequences(sequences, dimension):
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0  # when we have a word set its index values to 1
+    return results
+
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+# plot frequency of words. word indices are sorted by frequency hence there are more
+# at the head of the graph
+plt.plot(train_data[0])
+plt.show()
+
+
+# create a baseline model
+baseline_model = keras.Sequential([
+    # input shape is only required to make .summary work
+    keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+baseline_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+baseline_model.summary()
+
+
+# train the baseline model
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+# The baseline_model should train to test accurancy 100% but validation accurancy is about 85%
+
+# create smaller model with fewer hidden units
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(4, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+smaller_model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy', 'binary_crossentropy'])
+smaller_model.summary()
+
+# Now train smaller model
+smaller_history = smaller_model.fit(train_data,
+                                    train_labels,
+                                    epochs=20,
+                                    batch_size=512,
+                                    validation_data=(test_data, test_labels),
+                                    verbose=2)
+# smaller model has slightly less training accuracy, but similar validation accuracy
+
+# Now do an excessively large model
+bigger_model = keras.models.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+bigger_model.compile(optimizer='adam',
+                     loss='binary_crossentropy',
+                     metrics=['accuracy','binary_crossentropy'])
+
+bigger_model.summary()
+
+bigger_history = bigger_model.fit(train_data, train_labels,
+                                  epochs=20,
+                                  batch_size=512,
+                                  validation_data=(test_data, test_labels),
+                                  verbose=2)
+# This gets to 100% training accuracy in 6 epochs
+
+
+# Plot training and validation loss changes
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16,10))
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_' + key],
+                       '--', label=name.title() + ' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+                 label=name.title()+' Train')
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_', '').title())
+    plt.legend()
+    plt.xlim([0, max(history.epoch)])
+    plt.show()
+
+
+# Notice in the plot how the bigger network starts overfitting almost immediately,
+# whereas the smaller network is slower to overfit
+plot_history([('baseline', baseline_history),
+              ('smaller', smaller_history),
+              ('bigger', bigger_history)])
+
+# Now try using weight regularization to reduce overfitting -
+# L1 regularization - cost added per weight is proportional to the absolute value of weights
+# L2 regularization - cost added per weight is proportional to square of weights. (also called "weight decay")
+# L1 regularization tends to introduce sparsity of weights by driving weights to 0.
+# L2 regularization tends to penalize weights without making them sparse (small but non-zero values)
+# In keras weight regularization is added via parameters to the layers:
+l2_model = keras.models.Sequential([
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+l2_model.compile(optimizer='adam',
+                 loss='binary_crossentropy',
+                 metrics=['accuracy', 'binary_crossentropy'])
+l2_model_history = l2_model.fit(train_data, train_labels,
+                                epochs=20,
+                                batch_size=512,
+                                validation_data=(test_data, test_labels),
+                                verbose=2)
+# Final accuracy is similar, but training set accuracy is lower, and validation loss is less than baseline which is good
+
+# compare loss functions of baseline to l2 regularized
+plot_history([('baseline', baseline_history),
+              ('l2', l2_model_history)])
+
+# Another way of reducing overfitting is adding dropout, which induces 0 valued outputs at training time, and
+# scales down outputs by an equivalent amount at test/inference time.
+dpt_model = keras.models.Sequential([
+    keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+dpt_model.summary()
+dpt_model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy', 'binary_crossentropy'])
+dpt_model_history = dpt_model.fit(train_data, train_labels,
+                                  epochs=20,
+                                  batch_size=512,
+                                  validation_data=(test_data, test_labels),
+                                  verbose=2)
+
+# plot and compare history of dropout vs baseline vs l2
+plot_history([('baseline', baseline_history),
+              ('dropout', dpt_model_history),
+              ('l2', l2_model_history)])
+
